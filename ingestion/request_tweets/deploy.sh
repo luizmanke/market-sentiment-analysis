4f66ace70ec48904f462c2387971f195ec4280fe
luizmanke <luizmanke@gmail.com>
2020-10-25 20:11:57 -0300

    fix(request_tweets): Rename environment variables in deploy.sh

--- ingestion/request_tweets/deploy.sh
+++ ingestion/request_tweets/deploy.sh
@@ -8,10 +8,10 @@ gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
 # Deploy
 echo "GOOGLE_CREDENTIALS: '$GOOGLE_CREDENTIALS'" >> .env.yaml
 echo "GOOGLE_PROJECT_ID: '$GOOGLE_PROJECT_ID'" >> .env.yaml
-echo "GOOGLE_CREDENTIALS: '$TWITTER_ACCESS_TOKEN_KEY'" >> .env.yaml
-echo "GOOGLE_PROJECT_ID: '$TWITTER_ACCESS_TOKEN_SECRET'" >> .env.yaml
-echo "GOOGLE_CREDENTIALS: '$TWITTER_CONSUMER_KEY'" >> .env.yaml
-echo "GOOGLE_PROJECT_ID: '$TWITTER_CONSUMER_SECRET'" >> .env.yaml
+echo "TWITTER_ACCESS_TOKEN_KEY: '$TWITTER_ACCESS_TOKEN_KEY'" >> .env.yaml
+echo "TWITTER_ACCESS_TOKEN_SECRET: '$TWITTER_ACCESS_TOKEN_SECRET'" >> .env.yaml
+echo "TWITTER_CONSUMER_KEY: '$TWITTER_CONSUMER_KEY'" >> .env.yaml
+echo "TWITTER_CONSUMER_SECRET: '$TWITTER_CONSUMER_SECRET'" >> .env.yaml
 gcloud functions deploy request_tweets \
     --project $GOOGLE_PROJECT_ID \
     --entry-point run \
