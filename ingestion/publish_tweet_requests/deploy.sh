15ecf4dc21997145fe58ad3a9a52db279e75d896
luizmanke <luizmanke@gmail.com>
2020-10-25 10:08:59 -0300

    fix(publish_tweet_requests): Deploy function variables with a .env.yaml file

--- ingestion/publish_tweet_requests/deploy.sh
+++ ingestion/publish_tweet_requests/deploy.sh
@@ -1,9 +1,14 @@
+# Authenticate
 echo $GOOGLE_CREDENTIALS > /tmp/$CI_PIPELINE_ID.json
 gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
+
+# Deploy
+echo "GOOGLE_CREDENTIALS: '$GOOGLE_CREDENTIALS'" >> .env.yaml
+echo "GOOGLE_PROJECT_ID: '$GOOGLE_PROJECT_ID'" >> .env.yaml
 gcloud functions deploy publish_tweet_requests \
     --entry-point run \
     --runtime python38 \
     --trigger-topic tweet-request-trigger \
     --memory 128 \
     --timeout 60 \
-    --set-env-vars GOOGLE_CREDENTIALS=$GOOGLE_CREDENTIALS,GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID
+    --env-vars-file .env.yaml
