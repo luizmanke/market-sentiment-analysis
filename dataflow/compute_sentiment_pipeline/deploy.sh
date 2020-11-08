7291cf9edb58f412404a20572699279d42076626
luizmanke <luizmanke@gmail.com>
2020-11-08 02:49:11 -0300

    fix(compute_sentiment_pipeline): Add google service account email to deploy

--- dataflow/compute_sentiment_pipeline/deploy.sh
+++ dataflow/compute_sentiment_pipeline/deploy.sh
@@ -6,15 +6,12 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-# Authenticate
-echo $GOOGLE_CREDENTIALS > /tmp/$CI_PIPELINE_ID.json
-gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
-
 # Deploy
 python -m main \
     --runner DataflowRunner \
     --project $GOOGLE_PROJECT_ID \
     --region $GOOGLE_REGION \
+    --service_account_email $GOOGLE_SERVICE_ACCOUNT_EMAIL \
     --staging_location gs://$GOOGLE_PROJECT_ID/dataflows/staging \
     --temp_location gs://$GOOGLE_PROJECT_ID/dataflows/temp \
     --template_location gs://$GOOGLE_PROJECT_ID/dataflows/templates/compute_sentiment
